a70c9c6a1262894790ba92975c5d2e231d49db1a
OubaidAhmed <88501135+OubaidAhmed@users.noreply.github.com>
2024-02-01 00:49:43 +0500

    testing

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('https://blog-app-lake-beta.vercel.app/', (req, res) => res.send('Hello World!'))
 
 
 
