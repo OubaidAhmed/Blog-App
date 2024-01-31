87b9f3ff78d1b287944b9b05d37d41050734772d
OubaidAhmed <88501135+OubaidAhmed@users.noreply.github.com>
2024-02-01 01:29:53 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,15 +8,8 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 
-
-// const corsOptions = {
-//   origin: ["https://my-blog-app-rust.vercel.app"],
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
-// };
-
+// Use CORS globally
 app.use(cors());
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,26 +18,19 @@ app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
 app.get('/', (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-
-
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 const PORT = process.env.PORT || 8000;
 
 mongoose
   .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT))
-  .then(() =>
-    console.log(`Connected To Database and listening at PORT ${PORT}`)
-  )
-  .catch((err) => console.log(err));
-
+  .then(() => console.log(`Connected To Database and listening at PORT ${PORT}`))
+  .catch((err) => console.error(err));
