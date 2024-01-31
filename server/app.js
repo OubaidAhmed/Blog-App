9030640ac7a593eb24c5d5dc600c5ca8cc19aafa
OubaidAhmed <88501135+OubaidAhmed@users.noreply.github.com>
2024-01-31 22:21:55 +0500

    update vercel.json

--- server/app.js
+++ server/app.js
@@ -23,33 +23,10 @@ app.get('/', (req, res) => res.send('Hello World!'))
 const PORT = process.env.PORT || 8000;
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT))
   .then(() =>
     console.log(`Connected To Database and listening at PORT ${PORT}`)
   )
   .catch((err) => console.log(err));
 
-
-// Import required modules
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-
-// // Load environment variables from a .env file
-// dotenv.config();
-
-// // Create an Express app
-// const app = express();
-
-// // Connect to MongoDB
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-// app.get('/api/hello', (req, res) => res.send('Hello World!'))
-
-// // Start the server
-// const PORT = process.env.PORT || 8000;
-// app.listen(8000, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
