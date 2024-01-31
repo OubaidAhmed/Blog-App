65ae08824a8c2802b8d216344dd9f82f26f30c0d
OubaidAhmed <88501135+OubaidAhmed@users.noreply.github.com>
2024-02-01 00:56:15 +0500

    Few updates

--- server/app.js
+++ server/app.js
@@ -24,7 +24,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-app.get('https://blog-app-lake-beta.vercel.app/', (req, res) => res.send('Hello World!'))
+app.get('https://blog-app-lake-beta.vercel.app/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  res.send('Hello World!')
+})
 
 
 
