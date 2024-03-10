96cdcb780182800dd2861553bd47435edd82cfdf
OubaidAhmed <88501135+OubaidAhmed@users.noreply.github.com>
2024-03-10 20:35:48 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,3 +1,56 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import blogRouter from "./routes/blog-routes.js";
+// import router from "./routes/user-routes.js";
+// import cors from "cors";
+// import path from "path";
+// import { fileURLToPath } from 'url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// dotenv.config();
+// const app = express();
+
+// app.use(cors());
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.get("/", (req, res) => {
+//     app.use(express.static(path.resolve(__dirname, "client", "build")));
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
+
+// app.use("/api/user", router);
+// app.use("/api/blog", blogRouter);
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('Something went wrong!');
+// });
+
+// const PORT = 3000 || process.env.PORT;
+
+
+
+// mongoose.set('strictQuery', false);
+
+// mongoose
+//     .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//         console.log(`Connected To Database`);
+//         app.listen(PORT, () => {
+//             console.log(`Server is listening at PORT ${PORT}`);
+//         });
+//     })
+//     .catch((err) => console.log("MongoDB not connected"));
+
+// export { app };
+
+
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -14,31 +67,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "client", "build")));
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
-// Add a wildcard (*) route handler for unmatched routes
-// app.use((req, res, next) => {
-//     res.status(404).send("Not Found");
-// });
-
-app.use("/api/user", router);
-app.use("/api/blog", blogRouter);
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
-
-const PORT = 3000 || process.env.PORT;
-
-
 
 mongoose.set('strictQuery', false);
 
@@ -46,10 +76,26 @@ mongoose
     .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log(`Connected To Database`);
+
+        app.get("/", (req, res) => {
+            res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        });
+
+        app.use("/api/user", router);
+        app.use("/api/blog", blogRouter);
+
+        app.use(express.static(path.resolve(__dirname, "client", "build")));
+
+        app.use((err, req, res, next) => {
+            console.error(err.stack);
+            res.status(500).send('Something went wrong!');
+        });
+
+        const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
             console.log(`Server is listening at PORT ${PORT}`);
         });
     })
     .catch((err) => console.log("MongoDB not connected"));
 
-export { app }; 
+export { app };
